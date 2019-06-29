a299b008632b6d8883f7ce438e3bbe366e201f12
varon <varon@users.noreply.github.com>
2019-06-30 00:20:43 +0200

    rename for more common sense names

---
+++ build.cmd
@@ -0,0 +1,7 @@
+
+REM Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
+
+SET PATH=%LOCALAPPDATA%\Microsoft\dotnet;%PATH%
+dotnet restore build-boostrap.csproj
+dotnet fake run build.fsx%*
