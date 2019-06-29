b28b159d66b3aeaba9c986f1f9ef915b163b72c3
varon <varon@users.noreply.github.com>
2019-06-30 01:20:19 +0200

    re-enable dotnet bootstrap

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e -o pipefail
 
 # Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
-# curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
 
 PATH="~/.dotnet:$PATH"
 dotnet restore build-bootstrap.csproj
